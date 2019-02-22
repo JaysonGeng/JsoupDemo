d44c392a35a0bf95cd4963f57f962bbe31bbfa9e
michaelod <1013619679@qq.com>
2019-02-22 19:48:41 +0800

    first blood

---
+++ src/main/java/com/jiujiu/JiujiudemoApplication.java
@@ -0,0 +1,14 @@
+package com.jiujiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiujiudemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiujiudemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/jiujiu/controller/MainController.java
@@ -0,0 +1,75 @@
+package com.jiujiu.controller;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author michaelod
+ * @date 2018/12/27
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String main() {
+
+        return "index";
+    }
+
+
+    @RequestMapping("/search")
+    public String search(@RequestParam("q") String q, @RequestParam("start") String start, Model model) {
+        if (start == null || "".equals(start)) {
+            start = "0";
+        }
+        Collection<String> urls = getUrls(q,start);
+
+        model.addAttribute("urls",urls);
+
+        return "result";
+    }
+
+    public Collection<String> getUrls(String q, String start) {
+
+        Collection<String>  urls = new ArrayList<>();
+
+        Connection.Response response = null;
+        try {
+            response = Jsoup.connect("https://ja.ukiyo-e.org/search")
+                    .method(Connection.Method.GET)
+                    .ignoreContentType(true)
+                    .data("q", q, "start", start)
+                    .userAgent("Mozilla/5.0 (X11; Linux x86_64…) Gecko/20100101 Firefox/59.0")
+                    .header("Referer", "http://bkjws.sdu.edu.cn/").execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String body = response.body();
+
+        Document doc = Jsoup.parse(body);
+        Elements element = doc.select(".row");
+        Elements linkss = element.select("img[src]");
+        for (Element link : linkss) {
+            String linkHref = link.attr("src");
+            String imgUrl = linkHref.replace("thumbs", "images");
+            urls.add(imgUrl);
+        }
+        return urls;
+    }
+
+}
